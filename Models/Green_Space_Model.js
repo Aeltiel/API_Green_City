32168d24a7e6058728040c555e05e35b9e5bbe74
Ingrid Stemer <ingrid.stemer@live.fr>
2025-01-15 10:24:14 +0100

    mofication du modèle greenSpace pour ajouter la ref à la collection des plantes

--- Models/Green_Space_Model.js
+++ Models/Green_Space_Model.js
@@ -1,13 +1,22 @@
 const mongoose = require("mongoose");
 
 const greenSpaceSchema = mongoose.Schema({
-    nom : {type: String, required: true, trim: true},
-    latitude: {type: Number, required: true, trim: true},
-    longitude : {type: Number, required: true, trim: true},
-    superficie: {type: Number, required: true, trim: true},
-    plantes: {type: Array, default : []},
-    responsable: {type: String, required: true, trim: true},
-    imageSpaceUrl : {type : String, required : true}
-})
+  nom: { type: String, required: true, trim: true },
+  latitude: { type: Number, required: true, trim: true },
+  longitude: { type: Number, required: true, trim: true },
+  superficie: { type: Number, required: true, trim: true },
+  plantes: {
+    type: Array,
+    default: [
+      {
+        //le type mongoose.Schema.Types.ObjectId est une référence vers un document dans une autre collection
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Plantes",
+      },
+    ],
+  },
+  responsable: { type: String, required: true, trim: true },
+  imageSpaceUrl: { type: String, required: true },
+});
 
-module.exports = mongoose.model("GreenSpace", greenSpaceSchema)
+module.exports = mongoose.model("GreenSpace", greenSpaceSchema);
